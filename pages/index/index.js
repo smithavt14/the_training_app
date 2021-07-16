80f7d723ab45e69738b1c46e7b13b034bacb2600
Alex Smith <smithavt14@gmail.com>
2021-07-16 20:43:59 +0800

    add some style to show

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,31 +4,64 @@ Page({
 
   },
 
-  sortDates: function (workouts) {
-    let sessions = []
-    workouts.forEach((workout) => {
-      let dateOptions = {weekday: "long", month: "long", day: "numeric"}
-      let date = new Date(workout.date_time).toLocaleDateString('en-us', dateOptions)
-      
-      let session = sessions.find(workout => workout.date === date);
-      
-      if (session) session['workouts'].push(workout)
-      else sessions.push({date: date, workouts: [workout]})
+  navigateToShow: function (e) {
+    const id = e.currentTarget.dataset.id;
+    wx.navigateTo({
+      url: `/pages/show/show?id=${id}`
     })
-    this.setData({workouts: sessions})
   },
-  
+
+  sortDates: function (workouts) {
+    let trainingDates = []
+    let options = {weekday: "long", month: "long", day: "numeric"}
+    let today = new Date().toLocaleDateString('en-us', options)
+    
+    workouts.forEach((workout) => {
+      let date = new Date(workout.date_time).toLocaleDateString('en-us', options)
+      // -- Check to see if this workout's day already exists in the trainingDates array
+      let existing_date = trainingDates.find(workout => workout.date === date);
+      let dateDiff = Math.floor((new Date(workout.date_time) - new Date()) / 1000 / 60 / 60 / 24)
+      
+      if (existing_date) {
+        existing_date['workouts'].push(workout)
+      } else {
+        trainingDates.push({date, workouts: [workout], dateDiff})
+      }
+    })
+
+    let positiveDates = trainingDates.filter(trainingDate => trainingDate.dateDiff >= 0 )
+    positiveDates = positiveDates.sort((a, b) => a.dateDiff - b.dateDiff)
+
+    console.log(positiveDates)
+
+    trainingDates = trainingDates.sort((a, b) => new Date(a.date) - new Date(b.date))
+    
+    this.setData({trainingDates})
+    this.scrollToToday()
+  },
+
+  scrollToToday: function () {
+    wx.pageScrollTo({
+      selector: '.top',
+      duration: 300,
+      success: res => console.log(res),
+      fail: err => console.log(err)
+    })
+  },
+
   getMyWorkouts: function () {
+    // Set Variables
     let user = wx.getStorageSync('user');
-    let id = user.id
+    let User = new wx.BaaS.TableObject('user');
+    let Attendees = new wx.BaaS.TableObject('attendees');
+    let id = user.id;
     let query = new wx.BaaS.Query();
 
-    query.compare('user', '=', `${id}`)
+    query.compare('user', '=', User.getWithoutData(id));
 
-    let Attendees = new wx.BaaS.TableObject('attendees')
-    Attendees.setQuery(query).expand(['workout']).find().then(res => {
+    Attendees.setQuery(query).limit(100).expand(['workout']).find().then(res => {
       let workouts = res.data.objects.map(attendee => attendee.workout);
-      this.sortDates(workouts)
+      this.sortDates(workouts);
     })
   },
   
---
+++ pages/show/show.js
@@ -0,0 +1,78 @@
+Page({
+    data: {
+        toggleContainer: false
+    },
+    
+    // ----- Lifecycle Functions -----
+    onLoad: function (options) {
+        this.getUser();
+        this.getWorkout(options.id);
+    },
+
+    // ----- Custom Functions -----
+
+    getWorkout: function (id) {
+        const Workout = new wx.BaaS.TableObject('workouts')
+
+        Workout.get(id).then(res => {
+            let workout = res.data;
+            let options = {weekday: "long", month: "long", day: "numeric"};
+
+            workout.date_time = new Date(workout.date_time).toLocaleDateString('en-us', options);
+            
+            this.setData({workout: res.data});
+            this.getAttendees(res.data.id);
+            // this.getReviews(res.data.id);
+        })
+    },
+
+    completeWorkout: function (e) {
+        const attendees = this.data.attendees; 
+        const myAttendee = attendees.find(attendee => attendee.is_user);
+
+        myAttendee['is_complete'] = true;
+
+        this.setData({attendees})
+    },
+
+    getUser: function () {
+        const user = wx.getStorageSync('user');
+        this.setData({user});
+    },
+
+    getAttendees: function (id) {
+        let Attendees = new wx.BaaS.TableObject('attendees');
+        let query = new wx.BaaS.Query();
+
+        query.compare('workout', '=', id);
+
+        Attendees.setQuery(query).expand(['user']).find().then(res => {
+            const attendees = res.data.objects;
+            
+            // Identify whether user is an attendee
+            const user = wx.getStorageSync('user');
+            const myAttendee = attendees.find(attendee => attendee.user.id === user.id);
+            if (myAttendee) myAttendee['is_user'] = true;
+            if (myAttendee) myAttendee['is_user'] = true;
+
+            // Create local data variable to easily access info
+            const is_user = !!myAttendee;
+
+            // Set whether or not the workout it complete
+            const is_complete =  myAttendee['is_complete']
+
+            this.setData({attendees, is_complete, is_user})
+
+            wx.pageScrollTo({
+                scrollTop: 100,
+                duration: 300
+            })
+        })
+    },
+
+    toggleContainer: function (e) {
+        let binary = parseInt(e.currentTarget.dataset.binary)
+        let toggleContainer = !!binary
+        this.setData({toggleContainer})
+    }
+})
